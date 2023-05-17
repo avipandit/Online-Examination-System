eaa883adae1ab129f17c6e4f3c888f6341d9f583
avinash <avipandit9955876723@gmail.com>
2023-05-17 20:26:29 +0530

    added

---
+++ about.php
@@ -0,0 +1,11 @@
+<?php
+print "<h2><center>!!About Online Examination System!!</center></h2>";
+
+print "<p><center>In particular, this Exam Management System project in PHP MySQL focuses mainly on planning online MCQ examinations. To be more precise, the system helps to keep track of MCQs, and their respective answers. Also, the system displays all the available exam questions with given choices. In addition, the system allows online student registration.
+ Evidently, this project contains an admin panel with a student panel as well.
+  In an overview of this web application, students have to register in order to use the services.
+   Here, students can view simply start the examination at any time period. 
+   In terms of examination, he/she can have to click on the correct check box regarding the MCQ examination.
+    At last, the system displays the total score of the student. 
+    Besides, the user can check all the answers at the end of each test.</center></p>";
+?>
---
+++ inc/footer.php
@@ -0,0 +1,40 @@
+</body>
+
+<!-- Footer -->
+<section id="footer">
+	<div class="container">
+		<div class="row text-center text-xs-center text-sm-center text-md-center">
+			<div class="col-xs-12 col-sm-12 col-md-12">
+				<h5>Quick links</h5>
+				<ul class="list-unstyled quick-links">
+					<li><a href="register.php"><i class="fa fa-angle-double-right"></i>Home</a></li>
+					<li><a href="about.php"><i class="fa fa-angle-double-right"></i>About</a></li>
+
+				</ul>
+			</div>
+
+		</div>
+		<div class="row">
+			<div class="col-xs-12 col-sm-12 col-md-12 mt-2 mt-sm-5">
+				<ul class="list-unstyled list-inline social text-center">
+					<li class="list-inline-item"><a href="https://www.facebook.com/" target="_blank"><i class="fa fa-facebook"></i></a></li>
+					<li class="list-inline-item"><a href="https://twitter.com/" target="_blank"><i class="fa fa-twitter"></i></a></li>
+					<li class="list-inline-item"><a href="https://www.instagram.com/?hl=en" target="_blank"><i class="fa fa-instagram"></i></a></li>
+					<li class="list-inline-item"><a href="https://www.google.com/intl/en-GB/gmail/about/" target="_blank"><i class="fa fa-envelope"></i></a></li>
+				</ul>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-xs-12 col-sm-12 col-md-12 mt-2 mt-sm-2 text-center text-white">
+				<p class="h6">Developed by UCET Quad team</p>
+			</div>
+			</hr>
+		</div>
+	</div>
+</section>
+<!-- ./Footer -->
+
+
+<script src="vendor/jquery/jquery.min.js"></script>
+<script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+</footer>
